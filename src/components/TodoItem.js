828c8fdc0d834b0131d4d67739d7968591e88d25
Quoc Dat <buiquocdatgl2000@gmai.com>
2021-07-18 13:25:29 +0700

    Learn React

---
+++ src/components/TodoItem.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TodoItem = props => {
+    const todo = props.todoProps
+    const markComplete = props.markCompleteFunc
+    const deleTodo = props.deleTodoFunc
+
+    //Styles
+    const todoItemStyle ={
+        background: '#f4f4f4',
+        padding: '10px',
+        borderBottom: '1px #ccc dotted',
+        textDecoration: todo.completed ? 'line-through' : 'none'
+    }
+
+    const deleteButtonStyle ={
+        background: '#ff0000',
+        color: '#fff',
+        border: 'none',
+        padding: '5px 9px',
+        borderRadius: '50%',
+        cusor: 'pointer',
+        float: 'right'
+    }
+
+    return (
+        <p style={todoItemStyle}>
+            <input 
+                type="checkbox" 
+                onChange={markComplete.bind(this, todo.id)} 
+                checked={todo.completed}
+            />
+            {todo.title}
+            <button style={deleteButtonStyle} onClick={deleTodo.bind(this, todo.id)}>
+                Delete
+            </button>
+        </p>
+    )
+}
+
+//Khai Bao PropTypes
+TodoItem.propTypes = {
+    todoProps: PropTypes.object.isRequired,
+    markCompleteFunc: PropTypes.func.isRequired,
+    deleTodoFunc: PropTypes.func.isRequired
+}
+
+export default TodoItem
+
